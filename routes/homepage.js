399d673b3765dec733da2478d59fcb5f7d2249d1
Kirk Srijongsirikul <srijongsirikul.kirk@gmail.com>
2016-02-11 23:23:59 +0000

    added homepage handlebars

---
+++ routes/homepage.js
@@ -0,0 +1,7 @@
+exports.view = function(req, res){
+	console.log("TEST");
+	res.render('homepage');
+		/*'name': 'Fake friend',
+		'imageURL': 'http://lorempixel.com/500/500/people',
+		'description': 'All my friends are imaginary'*/
+};
