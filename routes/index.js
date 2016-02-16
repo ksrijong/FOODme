63c320a8b763dc1643c11092b2cf982eb2e8a463
Kirk Srijongsirikul <srijongsirikul.kirk@gmail.com>
2016-02-16 23:05:59 +0000

    edited fblogin.js

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,4 @@
 exports.view = function(req, res){
 	console.log("TEST");
-	res.render('index');
-		/*'name': 'Fake friend',
-		'imageURL': 'http://lorempixel.com/500/500/people',
-		'description': 'All my friends are imaginary'*/
+	res.render('index');		
 };
