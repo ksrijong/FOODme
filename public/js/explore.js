f9af77885837c088ab4a9d47c93198b6b1fd85be
Kirk Srijongsirikul <srijongsirikul.kirk@gmail.com>
2016-02-19 01:06:25 +0000

    cleaned up explore.js

--- public/js/explore.js
+++ public/js/explore.js
@@ -14,48 +14,7 @@
 $(document).ready(function() {
 
     //get JSON data with first
-    console.log("BEFORE EXPLORE!");
-
-    // OLD STUFF START
-
-    $.getJSON('../json/explore.json', function(data) {
-      dishes = data;
-      console.log("getJSON activated!");
-
-      curr = dishes.dishes[i];
-      console.log(curr);
-      var name = curr.dishName;
-      console.log("name variable testing..." + name);
-      var im = curr.imgURL;
-      console.log(im);
-      $('#dish_img').attr('src', im);
-      //setting up for review page
-      $('#dishIMG').attr('src', im);
-      $('item').text(name);
-      $('#dis').text(curr.dis);
-      $('#meh').text(curr.meh);
-      $('#yay').text(curr.yay);
-      $('#u1').text(curr.u1);
-      $('#u2').text(curr.u2);
-      $('#u3').text(curr.u3);
-      $('#d1').text(curr.d1);
-      $('#d2').text(curr.d2);
-      $('#d3').text(curr.d3);
-      $('#restaurant').text(curr.restaurantName);
-      $('#dish_name').text(curr.dishName);
-      $('#dname').html(curr.dishName);
-      $('#drest').html(curr.restaurantName);
-      console.log(curr.dishName);
-    });
-
-    //get JSON data of bookmarks
-    $.getJSON('../json/bookmarks.json', function(data) {
-      bookmarks = data;
-      console.log("getJSON activated!");
-      console.log(bookmarks);
-    });
-
-    // OLD STUFF END
+    console.log("BEFORE EXPLORE!");    
 
     //get json array data
     $.get('/addExplore', function(data) {
